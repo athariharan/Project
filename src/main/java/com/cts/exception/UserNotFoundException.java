2ff7f78d5bfa597367912380a1b884bbf6062c08
athariharan <Hariharan.AT@cognizant.com>
2026-04-02 22:07:29 +0530

    Delete src directory

--- src/main/java/com/cts/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.cts.exception;
-
-public class UserNotFoundException extends RuntimeException{
-	public UserNotFoundException(String msg) {
-		super(msg);
-	}
-}
--- src/main/java/com/cts/repository/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.cts.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.cts.entity.User;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-	boolean existsByEmail(String email);
-}
